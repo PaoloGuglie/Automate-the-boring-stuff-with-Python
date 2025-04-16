e85f8d0c76efff131f849381fb421ec790a3e762
Your Name <paologugliel@gmail.com>
2025-04-16 11:26:10 +0200

    corrected errors in code.

--- Random quiz files/quiz_generator.py
+++ Random quiz files/quiz_generator.py
@@ -5,7 +5,7 @@ import os
 from random import shuffle, sample
 
 
-def get_capitals() -> json:
+def get_capitals() -> dict:
 
     with open('capitals.json', 'r') as file:
         content = json.load(file)
@@ -13,15 +13,24 @@ def get_capitals() -> json:
     return content['capitals']
 
 
+def create_quiz_files_directory() -> None:
+
+    try:
+        os.mkdir('quiz_files')
+
+    except FileExistsError:
+        pass
+
+
 def main() -> None:
 
     capitals = get_capitals()
 
+    create_quiz_files_directory()
+
     for i in range(FILES_TO_CREATE):
 
         # Create the files
-        try: os.mkdir('quiz_files')
-        except FileExistsError: pass
         quiz_file = open(f"quiz_files\\Capitals_questions_{i + 1}.txt", 'w')
         answer_file = open(f"quiz_files\\Capitals_answers_{i + 1}.txt", 'w')
 
